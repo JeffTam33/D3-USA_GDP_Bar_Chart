4ca39ac44994cbdb0b8e1d027cb940f2205e17b5
JeffTam33 <53273407+JeffTam33@users.noreply.github.com>
2022-05-14 16:24:23 -0400

    Update js.js

--- js.js
+++ js.js
@@ -4,6 +4,7 @@
 const width = 1200;
 const height = 550;
 const padding = 50;
+const circleRadius = 5;
 
 var tooltips = d3.select(".visual-data")
                  .append("div")
@@ -23,16 +24,24 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
                 //Used for finding min and max of said data
                 var yearArr = data.map(i => i.Year);
                 var timeArr = data.map(i => i.Time);
+                //Used for y-axis-data
+                var dateObj = timeArr.map(time => {
+                  let minute = time.split(":")[0];
+                  let seconds = time.split(":")[1];
+                  let newDateObj = new Date();
+                  return new Date (newDateObj.setMinutes(minute, seconds));
+                })
+                //Used for position of y-axis data
                 timeArr = timeArr.map(time => {
                   let temp = time.split(":")
                   return parseInt(temp[0])*60 + parseInt(temp[1])
                 })
                 //Scales
                 const xScale = d3.scaleLinear()
-                                 .domain([d3.min(yearArr), d3.max(yearArr) + 1])
+                                 .domain([d3.min(yearArr) -1, d3.max(yearArr) + 1])
                                  .range([padding, width - padding])
                 const yScale = d3.scaleLinear()
-                                 .domain([minS, maxS])
+                                 .domain([d3.min(timeArr), d3.max(timeArr)])
                                  .range([height - padding, padding])
                 //Axises
                 const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
@@ -52,5 +61,45 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
                             .attr("id", "y-axis")
                             .attr("transform", "translate(" + padding +", 0)")
                             .call(yAxis);
-                
+                                //X-axis title
+                svgContainer.append("text")
+                            .attr("id", "y-axis-title")
+                            .attr("x", width / 2)
+                            .attr("y", height - (padding / 4))
+                            .text("Year");
+                //Y-axis title
+                svgContainer.append("text")
+                            .attr("id", "x-axis-title")
+                            .attr("x", (height * -1) + padding * 4)
+                            .attr("y", padding / 4)
+                            .attr("transform", "rotate(-90)")
+                            .text("Time (Minutes:Seconds)");
+                //Scatter Plot Data Appear
+                svgContainer.selectAll("svg")
+                            .data(data)
+                            .enter()
+                            .append("circle")
+                            .attr("class", "dot")
+                            .attr("reason", (d, i) => data[i].Doping)
+                            .attr("cx", (d, i) => xScale(yearArr[i]))
+                            .attr("data-xvalue", (d, i) => yearArr[i])
+                            .attr("cy", (d, i) => yScale(timeArr[i]))
+                            .attr("data-yvalue", (d, i) => dateObj[i])
+                            .attr("r", circleRadius)
+                            .on("mouseover", function (event, d){
+                              let reason = this.getAttribute("reason");
+                              tooltips.transition().duration(0);                             
+                              tooltips.attr("id", "legend")
+                                      .style("height", "100" + "px")
+                                      .style("width", "200" + "px")
+                                      .style("top", height - padding + "px")
+                                      .style("left", 50 + "px")
+                                      .style("opacity", .9)
+                                      .style("fill", "#FFFFFF")
+                                      .style("transform", "translateX(100px)")
+                                      .text(reason);
+                            })
+                            .on("mouseout", function(){
+                              tooltips.transition().duration(100).style("opacity", 0)
+                            })
 })
