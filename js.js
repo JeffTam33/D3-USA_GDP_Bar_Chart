2602c984325d455e3df8dac9f3849137b0d85ab5
JeffTam33 <53273407+JeffTam33@users.noreply.github.com>
2022-05-15 20:43:36 -0400

    Update/Polish css and js

--- js.js
+++ js.js
@@ -4,13 +4,19 @@
 const width = 1200;
 const height = 550;
 const padding = 50;
-const circleRadius = 5;
+const circleRadius = 8;
 
 var tooltips = d3.select(".visual-data")
                  .append("div")
                  .attr("id", "tooltip")
                  .attr("class", "tooltips")
                  .style("opacity", 1);
+
+var legend = d3.select(".visual-data")
+               .append("div")
+               .attr("id", "legend")
+               .attr("class", "legend")
+               .style("opacity", 1)
 
 var svgContainer = d3.select(".visual-data")
             .append("svg")
@@ -30,12 +36,12 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
                   let seconds = time.split(":")[1];
                   let newDateObj = new Date();
                   return new Date (newDateObj.setMinutes(minute, seconds));
-                })
+                });
                 //Used for position of y-axis data
                 timeArr = timeArr.map(time => {
                   let temp = time.split(":")
                   return parseInt(temp[0])*60 + parseInt(temp[1])
-                })
+                });
                 //Scales
                 const xScale = d3.scaleLinear()
                                  .domain([d3.min(yearArr) -1, d3.max(yearArr) + 1])
@@ -51,55 +57,77 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
                     return temp + "0"
                   }
                   return temp;
-                });
+                })
                 //Call Axis
                 svgContainer.append("g")
                             .attr("id", "x-axis")
                             .attr("transform", "translate(0, " + (height - padding) + ")")
-                            .call(xAxis);
+                            .call(xAxis)
                 svgContainer.append("g")
                             .attr("id", "y-axis")
                             .attr("transform", "translate(" + padding +", 0)")
-                            .call(yAxis);
+                            .call(yAxis)
                                 //X-axis title
                 svgContainer.append("text")
                             .attr("id", "y-axis-title")
                             .attr("x", width / 2)
                             .attr("y", height - (padding / 4))
-                            .text("Year");
+                            .text("Year")
                 //Y-axis title
                 svgContainer.append("text")
                             .attr("id", "x-axis-title")
                             .attr("x", (height * -1) + padding * 4)
                             .attr("y", padding / 4)
                             .attr("transform", "rotate(-90)")
-                            .text("Time (Minutes:Seconds)");
+                            .text("Time (Minutes:Seconds)")
                 //Scatter Plot Data Appear
                 svgContainer.selectAll("svg")
                             .data(data)
                             .enter()
                             .append("circle")
                             .attr("class", "dot")
+                            .attr("index", (d, i) => i)
                             .attr("reason", (d, i) => data[i].Doping)
                             .attr("cx", (d, i) => xScale(yearArr[i]))
                             .attr("data-xvalue", (d, i) => yearArr[i])
                             .attr("cy", (d, i) => yScale(timeArr[i]))
                             .attr("data-yvalue", (d, i) => dateObj[i])
                             .attr("r", circleRadius)
+                            .attr("fill", (d, i) => {
+                              if(data[i].Doping === ""){
+                                return "#2E86AB"
+                              }else{
+                                return "#F24236";
+                              }
+                            })
                             .on("mouseover", function (event, d){
+                              let i = this.getAttribute("index");
                               let reason = this.getAttribute("reason");
                               tooltips.transition().duration(0);                             
-                              tooltips.attr("id", "legend")
+                              tooltips.attr("data-year", this.getAttribute("data-xvalue"))
                                       .style("height", "100" + "px")
                                       .style("width", "200" + "px")
-                                      .style("top", height - padding + "px")
-                                      .style("left", 50 + "px")
+                                      .style("top", yScale(timeArr[i]) + padding / 2 + "px")
+                                      .style("left", xScale(yearArr[i]) + "px")
                                       .style("opacity", .9)
                                       .style("fill", "#FFFFFF")
                                       .style("transform", "translateX(100px)")
-                                      .text(reason);
+                                      .text(function (){
+                                            let info = "Name: " + data[i].Name + 
+                                                "\nYear: " + 
+                                                this.getAttribute("data-year") + 
+                                                "\n Time: " + 
+                                                data[i].Time
+                                            if(reason === ""){
+                                              return info + " Allegations: " + "No doping allegations"
+                                            }else{
+                                              return  info + " Allegations: " + reason
+                                        }
+                                           })
+                                      
                             })
                             .on("mouseout", function(){
                               tooltips.transition().duration(100).style("opacity", 0)
                             })
+  
 })
